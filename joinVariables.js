648c4cd087fcde38b2c8f1064aedd48bd2b4e6ed
Nyasha (Nash) Nziramasanga <51049696+Nyasha-Nziboi@users.noreply.github.com>
2019-07-05 00:24:08 +1000

    JoiningItems

---
+++ joinVariables.js
@@ -0,0 +1,9 @@
+// Using the join function to join arrays
+
+const firstName = 'Nyasha';
+const middleName = 'Nash';
+const lastName = 'Nziramasanga';
+fullNameArray = [ firstName, middleName, lastName ];
+fullName = fullNameArray.join(' '); //joins using a space
+console.log(fullName);
+//Output: Nyasha Nash Nziramasanga
